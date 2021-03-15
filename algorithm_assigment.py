9024a15d87cc315363640abc09a2a16632d48f22
danie <danielrgc97@gmail.com>
2021-03-15 10:41:37 +0100

    last path fixed

--- algorithm_assigment.py
+++ algorithm_assigment.py
@@ -3,9 +3,9 @@ import numpy as np
 
 class MyClass(object):
     def __init__(self):
-        # Extracting values from the file
+        # Importing graph data
         lines = []
-        f = open("./data_cases/case_04.in", "r")
+        f = open("./data_cases/case_06.in", "r")
         lines = f.readlines()
         self.first_line = [int(i) for i in lines[0].split(" ")]
         self.links = np.zeros([self.first_line[0],2])
@@ -13,21 +13,26 @@ class MyClass(object):
             nums_fila = [int(j) for j in lines[i].split(" ")]
             self.links[i-1,0] = nums_fila[0]
             self.links[i-1,1] = nums_fila[1]
-        self.reds_line = [int(i) for i in lines[len(lines)-2].split(" ")] 
+        string = lines[int(len(lines)-2)]
+        if lines[int(len(lines)-2)][len(lines[int(len(lines)-2)]) - 2] == ' ': 
+            lines[int(len(lines)-2)] = lines[int(len(lines)-2)][:-2]
+        self.reds_line = [int(i) for i in lines[len(lines)-2].split(" ")]
+        if lines[int(len(lines)-1)][len(lines[int(len(lines)-1)]) - 2] == ' ': 
+            lines[int(len(lines)-1)] = lines[int(len(lines)-1)][:-2]
         self.blues_line = [int(i) for i in lines[len(lines)-1].split(" ")]
 
-        # Variables for the algorithm
+        # Variables
         self.orderedBuffer = 0
         self.link_ways = 0
         self.link_states = 0
         self.flagRB = True
         self.toRemove = []
-        self.waysNumber = 1
+        self.waysNumber = 0
         self.tamOriginal = self.first_line[0]
+        self.marcados=0
 
-    # Functions
+    # Primary functions
     def graphWaysConstruction(self):
-        
         self.orderedBuffer = (np.zeros([self.tamOriginal + 1, 3]) - 1)
         self.link_ways = (np.zeros([self.first_line[0],4]))
 
@@ -61,21 +66,6 @@ class MyClass(object):
                 j += 1
             self.link_ways[i,2] = line[0]
             self.link_ways[i,3] = line[1]
-
-
-        # self.link_ways = (np.zeros([self.first_line[0],4]) - 1)
-        # for i in range(0,self.first_line[0]):
-        #     for j in range(0,self.first_line[0]):
-        #         if i != j:
-        #             x = 0.1
-        #             if self.links[i,0] == self.links[j,0] or self.links[i,0] == self.links[j,1]:
-        #                 if self.links[i,0] == self.links[j,1]: x = 0.2
-        #                 if self.link_ways[i,0] == -1 : self.link_ways[i,0] = j + x
-        #                 elif self.link_ways[i,1] == -1 : self.link_ways[i,1] = j + x
-        #             if self.links[i,1] == self.links[j,0] or self.links[i,1] == self.links[j,1]:
-        #                 if self.links[i,1] == self.links[j,1]: x = 0.2
-        #                 if self.link_ways[i,2] == -1 : self.link_ways[i,2] = j + x
-        #                 elif self.link_ways[i,3] == -1 : self.link_ways[i,3] = j + x
     def linkStatesCalculation(self):
         self.link_states = np.zeros([self.first_line[0],4])
         self.flagRB = True
@@ -84,30 +74,6 @@ class MyClass(object):
         self.flagRB = False
         for x in self.blues_line:
             self.markLinkRecursive(self.findColor(x))
-    def findColor(self,value):
-        position = np.where(self.links[:,0] == value)[0]
-        if len(position) > 0 : 
-            return np.ndarray.item(position + 0.1)
-        else: 
-            position = np.where(self.links[:,1] == value)[0]
-            return np.ndarray.item(position + 0.2)
-    def position(self,value):
-        value = round(value - 0.1, 1)
-        row = value // 1
-        col =  round(value % 1, 2) * 10
-        return [row,col]
-    def markLinkRecursive(self,value):
-        rowcol = self.position(value)
-        direction = rowcol[1]
-        if self.flagRB == False: direction += 2
-        if self.link_states[int(rowcol[0]),int(direction)] == 0:
-            self.link_states[int(rowcol[0]),int(direction)] = 1
-            if rowcol[1] == 0:
-                if self.link_ways[int(rowcol[0]),2] != -1: self.markLinkRecursive(self.link_ways[int(rowcol[0]),2])
-                if self.link_ways[int(rowcol[0]),3] != -1: self.markLinkRecursive(self.link_ways[int(rowcol[0]),3])
-            else:
-                if self.link_ways[int(rowcol[0]),0] != -1: self.markLinkRecursive(self.link_ways[int(rowcol[0]),0])
-                if self.link_ways[int(rowcol[0]),1] != -1: self.markLinkRecursive(self.link_ways[int(rowcol[0]),1])
     def findPaths(self):
         self.toRemove = []
         for i in range(0,self.first_line[0]):
@@ -124,8 +90,37 @@ class MyClass(object):
                 if np.any(self.link_states[int(link3)] == 0) and np.any(self.link_states[int(link4)] == 0):
                     self.toRemove.append(i+0.1)
                     self.waysNumber += 1
+        print("fin iteration, paths found:", len(self.toRemove))
         if len(self.toRemove) > 0: self.cutGraph()
+        else:
+            if len(self.reds_line) > 0 and len(self.blues_line) > 0 : self.waysNumber += 1
         return 0 < len(self.toRemove)
+    # Secondary functions
+    def findColor(self,value):
+        position = np.where(self.links[:,0] == value)[0]
+        if len(position) > 0 : 
+            return np.ndarray.item(position + 0.1)
+        else: 
+            position = np.where(self.links[:,1] == value)[0]
+            return np.ndarray.item(position + 0.2)
+    def position(self,value):
+        value = round(value - 0.1, 1)
+        row = value // 1
+        col =  round(value % 1, 2) * 10
+        return [row,col]
+    def markLinkRecursive(self,value):
+        self.marcados += 1
+        rowcol = self.position(value)
+        direction = rowcol[1]
+        if self.flagRB == False: direction += 2
+        if self.link_states[int(rowcol[0]),int(direction)] == 0:
+            self.link_states[int(rowcol[0]),int(direction)] = 1
+            if rowcol[1] == 0:
+                if self.link_ways[int(rowcol[0]),2] != -1: self.markLinkRecursive(self.link_ways[int(rowcol[0]),2])
+                if self.link_ways[int(rowcol[0]),3] != -1: self.markLinkRecursive(self.link_ways[int(rowcol[0]),3])
+            else:
+                if self.link_ways[int(rowcol[0]),0] != -1: self.markLinkRecursive(self.link_ways[int(rowcol[0]),0])
+                if self.link_ways[int(rowcol[0]),1] != -1: self.markLinkRecursive(self.link_ways[int(rowcol[0]),1])
     def cutGraph(self):
         for x in self.toRemove:
             self.findColorsRecursive(x)
@@ -152,7 +147,6 @@ class MyClass(object):
         position = np.where(self.blues_line == edge)[0]
         if len(position) > 0 :
             self.blues_line = np.delete(self.blues_line,position)
-
 
 
 a = MyClass()
